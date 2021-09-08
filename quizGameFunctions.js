483e65d9bce5f11fbdecbd1c4e8dc7e970e7a598
csluggers <csluggers@gmail.com>
2021-09-08 15:38:33 -0700

    sf

--- quizGameFunctions.js
+++ quizGameFunctions.js
@@ -277,6 +277,14 @@ function off() {
   document.getElementById("overlay").style.display = "none";
 	document.getElementById("lifeline2").style.display ="none";
 }
+function on2() {
+  document.getElementById("overlay2").style.display = "block";
+}
+
+function off3() {
+  document.getElementById("overlay2").style.display = "none";
+	document.getElementById("lifeline3").style.display ="none";
+}
 
 
 
